93c0087784af55d9184c9d29a6eb08375e5519b1
Vysakhacharya <141657455+Vysakhacharya@users.noreply.github.com>
2023-09-26 02:16:21 -0700

    Add files via upload

---
+++ bin/customwidgwt.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Widget App'),
+        ),
+        body: Center(
+
+          child: CustomWidget(
+            labelText: 'Custom Button',
+            onPressed: () {
+              // Add your custom button logic here
+              print('Button Pressed');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomWidget extends StatelessWidget {
+  final String labelText;
+  final VoidCallback onPressed;
+
+  CustomWidget({required this.labelText, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(labelText),
+    );
+  }
+}
